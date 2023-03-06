3fcfb05bda01439cad941b9f7f1d9b2f1506c6e2
West <anton.zakharov@wdg.com.ua>
2023-03-06 09:15:30 +0200

    custom epg

---
+++ epgBuilder.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fs = require('fs');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const {ungzip} = require('node-gzip');
+const parser = require('xml2json');
+const mapId = require('./epgMap.json');
+const iptv = require('./iptv.json');
+
+module.exports = (async () => {
+    const url = 'https://iptvx.one/EPG_LITE';
+    const XMLdata = await fetch(url).then(a => a.blob()).then(a => a.arrayBuffer())
+    const xml = (await ungzip(XMLdata)).toString();
+    let jObj = parser.toJson(xml);
+
+    const res = JSON.parse(jObj);
+
+    res.tv.channel = res.tv.channel.filter(({id}) => {
+        if(Object.values(mapId).some(a => a === id)){
+            return true
+        }
+        return !!iptv.some(a => a.id === id);
+    })
+
+    res.tv.programme = res.tv.programme.filter(({channel}) => {
+        if(Object.values(mapId).some(a => a === channel)){
+            return true
+        }
+        return !!iptv.some(a => a.id === channel);
+    })
+
+
+    console.log('done');
+    fs.writeFileSync(path.join(__dirname, './epg.xml') , parser.toXml(res))
+    fs.writeFileSync(path.join(__dirname, './epg.json'), JSON.stringify(res))
+})()
